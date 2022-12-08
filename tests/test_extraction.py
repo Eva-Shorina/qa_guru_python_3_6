36d84d6bc76b60162e090da605194075f333551d
Eva-Shorina <eva.shorina@yandex.ru>
2022-12-08 20:58:50 +0300

    initial commit

---
+++ tests/test_extraction.py
@@ -0,0 +1,50 @@
+import shutil
+from zipfile import ZipFile
+import csv
+import os
+from PyPDF2 import PdfReader
+from openpyxl import load_workbook
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+root2 = os.path.dirname(this_dir)
+path = os.path.join(root2, 'resources')
+arch_path = os.path.join(path,'archive.zip')
+
+
+zip_ = ZipFile(arch_path)
+
+
+def test_csv():
+    file = zip_.extract(zip_.namelist()[1])
+
+    with open(file) as csvfile:
+        csvfile = csv.reader(csvfile)
+
+        for r in csvfile:
+            temp = r[0]
+
+        value = temp.split(';')[3]
+        assert value == 'CCCC'
+
+
+def test_pdf():
+    file = zip_.extract(zip_.namelist()[3])
+
+    reader = PdfReader(file)
+    page = reader.pages[0]
+    text = page.extract_text()
+    print(text)
+    assert 'ура' in text
+
+
+def test_xlsx():
+    file = zip_.extract(zip_.namelist()[4])
+    workbook = load_workbook(file)
+    sheet = workbook.active
+    assert sheet.cell(row=3, column=2).value == 'Eagle'
+
+def test_jpg():
+    file = zip_.extract(zip_.namelist()[2])
+    size = os.path.getsize(file)
+    assert size == 2435
+
---
+++ zippack.py
@@ -0,0 +1,14 @@
+import os
+import zipfile
+
+root = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(root, 'resources')
+file_dir = os.listdir(path)
+arch_path = os.path.join(path,'archive.zip')
+
+with zipfile.ZipFile(arch_path, mode='w', \
+                     compression=zipfile.ZIP_DEFLATED) as zf:
+    for file in file_dir:
+        add_file = os.path.join(path, file)
+        zf.write(add_file)
+
